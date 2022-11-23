dacd53b4176c01f3d879e4dde881907c05d0d79a
Priyanshu <priyanshubhardwaj19dec@gmail.com>
2022-11-24 00:01:57 +0530

    UI changes

--- src/StocksPage.js
+++ src/StocksPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import bull from "../src/assets/bull-image.png";
 
 function StocksPage() {
   const list = [
@@ -26,15 +27,57 @@ function StocksPage() {
   ];
 
   return (
-    <ul style={{ fontWeight: "bold" }}>
-      {list.map((item, index) => {
-        return (
-          <li key={index}>
-            <Link to={`/stocks/${item.url}`}>{item.name}</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div style={{ width: "100%", minHeight: "100vh", background: "#fff5f5" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src={bull} style={{ width: "80px" }} />
+          <div
+            style={{
+              fontWeight: "700",
+              letterSpacing: "1px",
+              fontSize: "18px",
+              color: "black",
+            }}
+          >
+            <span style={{ color: "red" }}>Investing</span>IQ
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "2vw",
+            color: "red",
+            fontWeight: "bold",
+          }}
+        >
+          {/* <div style={{ cursor: "pointer", letterSpacing: "1px" }}>
+            <Link style={{ color: "red", textDecoration: "none" }} to="/stocks">
+              Stocks
+            </Link>
+          </div>
+          <div style={{ cursor: "pointer", letterSpacing: "1px" }}>About</div>
+          <div style={{ cursor: "pointer", letterSpacing: "1px" }}>
+            Features
+          </div> */}
+        </div>
+      </div>
+      <ul style={{ fontWeight: "bold" }}>
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link to={`/stocks/${item.url}`}>{item.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
